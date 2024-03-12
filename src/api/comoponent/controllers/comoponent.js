42544e8c825c460ecd73eb00e3ce59645ff226c6
ricardo <ricardo.br.pi@gmail.com>
2024-03-11 22:35:38 -0300

    feat: update database

--- src/api/comoponent/controllers/comoponent.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * comoponent controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::comoponent.comoponent');
