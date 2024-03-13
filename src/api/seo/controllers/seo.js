b9f2aba5280895d8da39a8e6915a4d8e97a81c90
ricardo <ricardo.br.pi@gmail.com>
2024-03-12 21:12:57 -0300

    feat: single type seo

---
+++ src/api/seo/controllers/seo.js
@@ -0,0 +1,52 @@
+'use strict';
+
+/**
+ * seo controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+//module.exports = createCoreController('api::seo.seo');
+
+module.exports = createCoreController("api::seo.seo", ({ strapi }) => ({
+  async find(ctx) {
+    const seoAttributes = await strapi.entityService.findMany('api::seo.seo', {
+      fields: ['title', 'description'],
+      populate: ['OpenGraph', 'additionalLinkTags'],
+    });
+    const headerAttributes = await strapi.entityService.findMany('api::header.header', {
+      fields: ['title', 'subtitle', 'motovationalPhrase', 'image', 'button']
+    });
+    const skilltAttributes = await strapi.entityService.findMany('api::skill.skill', {
+      fields: ['title', 'subtitle'],
+      populate: ['Technologie'],
+    });
+    const factAttributes = await strapi.entityService.findMany('api::fact.fact', {
+      fields: ['title'],
+      populate: ['factsAbout'],
+    });
+    const educationAttributes = await strapi.entityService.findMany('api::education.education', {
+      fields: ['title', 'subtitle', 'faculty', 'course', 'state'],
+    });
+    const projectAttributes = await strapi.entityService.findMany('api::project.project', {
+      fields: ['title', 'subtitle', 'button'],
+      populate: ["Projects"],
+    });
+
+    const footerAttributes = await strapi.entityService.findMany('api::footer.footer', {
+      fields: ['title', 'subtitle', 'email', 'phone'],
+    });
+
+    const result = {
+      seo: seoAttributes,
+      header: headerAttributes,
+      skill: skilltAttributes,
+      fact: factAttributes,
+      education: educationAttributes,
+      project: projectAttributes,
+      footer: footerAttributes,
+    }
+
+    return result;
+  },
+}));
