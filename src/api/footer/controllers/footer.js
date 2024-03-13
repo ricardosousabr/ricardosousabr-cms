487eb1a8180cd47d17c85fe3c0677c0d0528e0ae
ricardo <ricardo.br.pi@gmail.com>
2024-03-12 21:13:46 -0300

    feat: remove the call from other footer controllers

--- src/api/footer/controllers/footer.js
+++ src/api/footer/controllers/footer.js
@@ -1,26 +1,10 @@
 'use strict';
 
-
 /**
  * footer controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController("api::footer.footer", ({ strapi }) => ({
-  async find(ctx) {
-    const footerAttributes = await strapi.db.query('api::footer.footer').findOne({
-      select: ['title', 'subtitle', 'email', 'phone'],
-    });
-    const educationAttributes = await strapi.db.query('api::education.education').findOne({
-      select: ['title', 'subtitle', 'faculty', 'course', 'state'],
-    });
+module.exports = createCoreController('api::footer.footer');
 
-    const result = {
-      footer: footerAttributes,
-      education: educationAttributes
-    }
-
-    return result;
-  },
-}));
